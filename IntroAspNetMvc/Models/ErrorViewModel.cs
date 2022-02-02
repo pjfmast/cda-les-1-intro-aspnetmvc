38d8c76c09e0f4c3906ce7118bb49e1c41fe6975
Paul de Mast <pjf.demast@avans.nl>
2022-02-02 16:42:07 +0100

    Add project files.

---
+++ IntroAspNetMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IntroAspNetMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
